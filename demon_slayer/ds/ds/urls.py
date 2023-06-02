a103425077ce8bdf3dfcbc00718824bd67b61be1
Tonima <tonimadas348@gmail.com>
2023-06-02 14:03:36 +0530

    added

--- demon_slayer/ds/ds/urls.py
+++ demon_slayer/ds/ds/urls.py
@@ -21,5 +21,6 @@ from . import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.index, name="index"),
-    path('hashiras/', include('hashira.urls'), name='hashiras')
+    path('hashiras/', include('hashira.urls'), name='hashiras'),
+    path('demons/', include('demons.urls'), name='demons')
 ]
