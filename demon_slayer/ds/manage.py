9cfdd2fb640e08de7339ccda1a675812d781acdf
Mrinmay Das <mrinmaydas2604@gmail.com>
2023-06-07 22:59:25 +0530

    pychache gone

--- demon_slayer/ds/manage.py
+++ demon_slayer/ds/manage.py
@@ -3,7 +3,7 @@
 import os
 import sys
 
-
+os.environ['PYTHONDONTWRITEBYTECODE'] = '1'
 def main():
     """Run administrative tasks."""
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ds.settings')
